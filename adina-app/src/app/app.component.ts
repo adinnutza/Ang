c344aaa3e774ca4733b5d697a0ed2feaba8a98cb
Ella <adina.caldare@gmail.com>
2017-10-29 16:14:56 +0200

    fisrt

---
+++ adina-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+ templateUrl: './app.component.html',
+ // template: '<app-servers></app-servers>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Adina';
+  name = "Bau";
+}
---
+++ adina-app/src/app/servers/servers.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+ // template: '<app-server></app-server><app-servers></app-servers>',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
